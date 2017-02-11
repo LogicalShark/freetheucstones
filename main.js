8d224e5904eba023aa1f778cb9f19ecfcbfb074f
LogicalShark <marcusa3.14@gmail.com>
2017-02-11 16:12:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,6 @@ function login()
 }
 function bossEncounter()
 { 
-  alert("A boss is approaching!");
   switch(Math.floor(Math.random()*5))//b
   {
     case 0: window.location.assign("battle.html");
@@ -52,34 +51,6 @@ function bossEncounter()
       break;
     default: window.location.assign("battle.html");
   }
-    var d;
-    var user = firebase.database().ref("users").child(username);
-    user.on("value", function(snapshot)
-               {
-      d = snapshot.val();
-      var x = d["stones"];
-      var b = d["bosses"];
-      switch(Math.floor(Math.random()*5))//b
-      {
-        case 0: window.location.assign("battle.html");
-          break;
-        case 1: window.location.assign("battle1.html");
-          break;
-        case 2: window.location.assign("battle2.html");
-          break;
-        case 3: window.location.assign("battle3.html");
-          break;
-        case 4: window.location.assign("battle4.html");
-          break;
-//         case 5: window.location.assign("battle5.html");
-//           break;
-        default: window.location.assign("battle.html");
-      }
-      user.update({
-        "stones": x,
-        "bosses": b+1
-      });
-    });
 }
 function freeStone()
 {
